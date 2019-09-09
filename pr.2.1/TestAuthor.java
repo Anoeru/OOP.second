aa6a3a5c6edf7b3901e79f91e159108dc6f5cce0
anoeru <47786730+Anoeru@users.noreply.github.com>
2019-09-09 17:42:10 +0300

    Add files via upload

---
+++ pr.2.1/TestAuthor.java
@@ -0,0 +1,8 @@
+package lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("sergei pavlov", "notimportant@mail.ru", 'm');
+        System.out.println(a1);
+    }
+}
